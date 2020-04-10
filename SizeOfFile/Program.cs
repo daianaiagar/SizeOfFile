7b47eb44285936cbb6cea6a36254509244d0c6d1
Daiana <daianaiagar@gmail.com>
2020-04-10 17:05:51 +0300

    Add project files.

---
+++ SizeOfFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SizeOfFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          StreamReader file = new StreamReader(@"..\..\input.txt");
+          const string filename = @"../../input.txt";
+          FileInfo info = new FileInfo(filename);
+          long lenght = info.Length;
+          Console.WriteLine($"size file :{lenght}");
+          file.Close();
+        }
+    }
+}
